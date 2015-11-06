f75b9c557a46bfb3c5cbabf976bdc7eb4a498d2f
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-05 23:59:23 -0800

    dont rely on ubuntu user during cleanup

--- script/cleanup.sh
+++ script/cleanup.sh
@@ -67,5 +67,3 @@ rm -f /EMPTY
 # Make sure we wait until all the data is written to disk, otherwise
 # Packer might quite too early before the large files are deleted
 sync
-
-find ~ubuntu | xargs ls -ltrhd
