ebacf24760645e771f434fc21a440478976a1072
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-23 22:04:25 +0000

    skip docker container if a docker

--- script/last.sh
+++ script/last.sh
@@ -3,27 +3,28 @@
 umask 022
 
 # install cloud-init last until TODO ssh breakage identified
-if [[ $PACKER_BUILDER_TYPE =~ virtualbox ]]; then
-  aptitude install -y cloud-init
+aptitude install -y cloud-init
+
+if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
+  # make a docker image of our selves
+  mount -o bind / /mnt
+
+  mkdir -p /tmp/meh/{tmp,docker}
+  mount -o bind /tmp/meh/tmp /mnt/tmp
+  mount -o bind /tmp/meh/docker /mnt/var/lib/docker
+
+  pushd /mnt
+  mkdir run/lock run/user
+  chmod 1777 tmp run/lock
+
+  tar -c . | docker import - ubuntu
+
+  popd
+
+  umount /mnt/tmp
+  umount /mnt/var/lib/docker
+  umount /mnt
 fi
 
-# make a docker image of our selves
-mount -o bind / /mnt
-
-mkdir -p /tmp/meh/{tmp,docker}
-mount -o bind /tmp/meh/tmp /mnt/tmp
-mount -o bind /tmp/meh/docker /mnt/var/lib/docker
-
-pushd /mnt
-mkdir run/lock run/user
-chmod 1777 tmp run/lock
-
-tar -c . | docker import - ubuntu
-
-popd
-
-umount /mnt/tmp
-umount /mnt/var/lib/docker
-umount /mnt
-
 sync
+
