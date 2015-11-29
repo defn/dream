3e7daaadc0c943f2f87ec537dc7f7f56ce969c1f
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-28 23:33:26 -0800

    install sshd

--- script/sshd.sh
+++ script/sshd.sh
@@ -1,5 +1,9 @@
 #!/bin/bash -eux
 
+apt-get install -y openssh-server
+
+install -d -o root -g root -m 0755  /var/run/sshd
+
 if [[ -f "/etc/ssh/sshd_config" ]]; then
   echo "UseDNS no" >> /etc/ssh/sshd_config
 fi
