ff8803208b9316539ae8f7dbed7a8c77894b194d
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-29 23:44:34 -0800

    bump max auth retries, add insecure key to docker root

--- script/sshd.sh
+++ script/sshd.sh
@@ -6,4 +6,5 @@ install -d -o root -g root -m 0755  /var/run/sshd
 
 if [[ -f "/etc/ssh/sshd_config" ]]; then
   echo "UseDNS no" >> /etc/ssh/sshd_config
+  echo "MaxAuthTries 20" >> /etc/ssh/sshd_config
 fi
