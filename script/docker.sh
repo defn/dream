b381ef60af91ec1b67a7010d5db0f8420ead4252
Tom Bombabil <amanibhavam@destructuring.org>
2015-11-30 23:17:21 -0800

    install server groups in docker

--- script/docker.sh
+++ script/docker.sh
@@ -4,6 +4,8 @@ SSH_USER=${SSH_USERNAME:-vagrant}
 SSH_PASS=${SSH_PASSWORD:-vagrant}
 
 if [[ $PACKER_BUILDER_TYPE =~ docker ]]; then
+  apt-get install -y minimal^ server^ standard^
+
   install -d -m 0700 -o ubuntu -g ubuntu ~ubuntu/.ssh 
   echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' > ~ubuntu/.ssh/authorized_keys
   chown ubuntu:ubuntu ~ubuntu/.ssh/authorized_keys
