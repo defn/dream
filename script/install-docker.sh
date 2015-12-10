a37bfa50d2ec6337a49d6dde9e0092f5dfd7767f
Tom Bombabil <amanibhavam@destructuring.org>
2015-12-10 14:02:10 -0800

    set some variables here

--- script/install-docker.sh
+++ script/install-docker.sh
@@ -16,6 +16,11 @@ docker_package_install() {
     # Install Docker
     curl -sSL https://get.docker.com/ | sh
 
+    # listen on tcp, use device mapper
+    cat | sudo tee -a /etc/default/docker <<"____EOF"
+DOCKER_OPTS="-H tcp://0.0.0.0: --storage-driver=devicemapper"
+____EOF
+
     # Enable memory and swap accounting
     sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
     update-grub
