ce33a54281aabf7b04d2a4c8d87b8d6dc704cfc4
Tom Bombabil <amanibhavam@destructuring.org>
2016-01-16 22:03:40 -0800

    move custom packages into site.sh

--- script/site.sh
+++ script/site.sh
@@ -89,3 +89,14 @@ if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
     docker pull ubuntu:trusty
   fi
 fi
+
+
+aptitude update
+
+aptitude -y install git vim unzip curl language-pack-en
+aptitude -y install python-setuptools python-dev libffi-dev libssl-dev
+aptitude -y install libreadline-dev
+aptitude -y install gcc-multilib g++-multilib
+aptitude -y install openjdk-7-jdk
+
+aptitude -y purge nano mlocate
