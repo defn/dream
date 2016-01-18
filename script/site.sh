c8bf55d1cea4915df18851bbed61a1e3395829e8
Tom Bombabil <amanibhavam@destructuring.org>
2016-01-18 01:37:47 -0800

    move java to leinengen

--- script/site.sh
+++ script/site.sh
@@ -74,16 +74,14 @@ aptitude -y install git vim unzip curl language-pack-en
 aptitude -y install python-setuptools python-dev libffi-dev libssl-dev
 aptitude -y install libreadline-dev
 aptitude -y install gcc-multilib g++-multilib
-aptitude -y install openjdk-7-jdk
+
 aptitude -y purge nano mlocate
 
-if [[ "$DOCKER" =~ ^(true|yes|on|1|TRUE|YES|ON])$ ]]; then
-  if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
-    if [[ ! -x "$(which docker 2>&- || true)" ]]; then
-      give_docker_non_root_access
-      docker_package_install 
-      gpasswd -a ubuntu docker
-      docker pull ubuntu:trusty
-    fi
+if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
+  if [[ ! -x "$(which docker 2>&- || true)" ]]; then
+    give_docker_non_root_access
+    docker_package_install 
+    gpasswd -a ubuntu docker
+    docker pull ubuntu:trusty
   fi
 fi
