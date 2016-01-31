c0a3a2ab04961a286a13a4868fdfcd6b6b0ecd49
Tom Bombabil <amanibhavam@destructuring.org>
2016-01-31 03:23:12 -0500

    moved docker install out to box project

--- script/site.sh
+++ script/site.sh
@@ -1,33 +1,4 @@
 #!/bin/bash
 
-give_docker_non_root_access() {
-    # Add the docker group if it doesn't already exist
-    groupadd docker
-}
-
-docker_package_install() {
-    # Get the latest docker package
-    curl -sSL https://get.docker.com/gpg | apt-key add -
-
-    # Install Docker
-    curl -sSL https://get.docker.com/ | sh
-
-    # listen on tcp, use device mapper
-    cat | tee -a /etc/default/docker <<"____EOF"
-DOCKER_OPTS="-H tcp://0.0.0.0: --storage-driver=devicemapper"
-____EOF
-
-    # Enable memory and swap accounting
-    sed -i 's/GRUB_CMDLINE_LINUX=""/GRUB_CMDLINE_LINUX="cgroup_enable=memory swapaccount=1"/' /etc/default/grub
-    update-grub
-}
-
 aptitude -y install git vim unzip curl language-pack-en
 aptitude -y purge nano mlocate
-
-if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
-  if [[ ! -x "$(which docker 2>&- || true)" ]]; then
-    give_docker_non_root_access
-    docker_package_install 
-  fi
-fi
