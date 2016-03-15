04827300af93f4a8244f87af45f7c6e01777f3b4
Tom Bombadil <amanibhavam@destructuring.org>
2016-03-15 11:22:46 -0700

    proxy is optional

--- script/first.sh
+++ script/first.sh
@@ -3,7 +3,9 @@
 env | grep -i proxy
 uname -a
 if [[ ! $PACKER_BUILDER_TYPE =~ amazon ]]; then
-  echo "Acquire::http::Proxy \"$http_proxy\";" | tee /etc/apt/apt.conf.d/99boxcache
+  if [[ -n "${http_proxy:-}" ]]; then
+    echo "Acquire::http::Proxy \"$http_proxy\";" | tee /etc/apt/apt.conf.d/99boxcache
+  fi
 fi
 apt-get install -y aptitude
 aptitude -y update > /dev/null
