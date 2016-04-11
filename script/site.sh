015e3828669fb072e886299d66f2b2f2785e3f5a
Tom Bombadil <amanibhavam@destructuring.org>
2016-04-11 13:59:22 -0700

    install zfs, too

--- script/site.sh
+++ script/site.sh
@@ -1,4 +1,18 @@
 #!/usr/bin/env bash
 
+source /etc/lsb-release
+
 aptitude -y install unzip curl language-pack-en lvm2 ruby git lxd
 aptitude -y purge nano mlocate
+
+case "$DISTRIB_CODENAME" in
+  trusty)
+    apt-add-repository -y ppa:zfs-native/stable
+    aptitude update
+    aptitude install -y ubuntu-zfs
+    ;;
+
+  xenial)
+    aptitude install -y zfsutils-linux
+    ;;
+esac
