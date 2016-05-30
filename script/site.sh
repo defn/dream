f120f8214ceea93c712cdc55286d102a94ff77fd
Tom Bombadil <amanibhavam@destructuring.org>
2016-05-29 18:07:53 -0700

    skip lxd, zfs on null builder

--- script/site.sh
+++ script/site.sh
@@ -8,14 +8,15 @@ aptitude install -y software-properties-common
 
 case "$DISTRIB_CODENAME" in
   trusty)
-    add-apt-repository ppa:ubuntu-lxc/lxd-stable
-    apt-add-repository -y ppa:zfs-native/stable
-    aptitude update
-    aptitude install -y lxd ubuntu-zfs dkms
+    if [[ ! $PACKER_BUILDER_TYPE =~ null ]]; then
+      add-apt-repository ppa:ubuntu-lxc/lxd-stable
+      apt-add-repository -y ppa:zfs-native/stable
+      aptitude update
+      aptitude install -y lxd ubuntu-zfs dkms
+    fi
     ;;
 
   xenial)
     aptitude install -y lxd zfsutils-linux
     ;;
 esac
-
