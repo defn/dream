2307391768b8aff2b3f6be474e4c872a0a2a9c0d
Tom Bombadil <amanibhavam@destructuring.org>
2016-05-29 19:26:47 -0700

    end of sshd_config might not have a newline

--- script/sshd.sh
+++ script/sshd.sh
@@ -5,6 +5,9 @@ aptitude install -y openssh-server
 install -d -o root -g root -m 0755  /var/run/sshd
 
 if [[ -f "/etc/ssh/sshd_config" ]]; then
-  echo "UseDNS no" >> /etc/ssh/sshd_config
-  echo "MaxAuthTries 20" >> /etc/ssh/sshd_config
+  {
+    echo
+    echo "UseDNS no"
+    echo "MaxAuthTries 20"
+  } >> /etc/ssh/sshd_config
 fi
