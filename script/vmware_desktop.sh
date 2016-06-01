d350ccf9642f6e38cd3434e70e01163e3aa4a61e
Tom Bombadil <amanibhavam@destructuring.org>
2016-05-31 22:04:58 -0700

    latest vmware wants to use open-vm-tools

--- script/vmware_desktop.sh
+++ script/vmware_desktop.sh
@@ -4,33 +4,5 @@ set -eux
 
 echo "==> Installing VMware Tools"
 
-pth_vmware_iso="$(ls -d /home/*/linux.iso 2>/dev/null || true)"
-if [[ -f "$pth_vmware_iso" ]]; then
-  cd "$(dirname "$pth_vmware_iso")"
-
-  if [[ -f "linux.iso" ]]; then
-    mount -o loop "$(pwd)/linux.iso" /mnt
-
-    # install
-    VMWARE_TOOLS_PATH=$(ls /mnt/VMwareTools-*.tar.gz)
-    VMWARE_TOOLS_VERSION=$(echo "${VMWARE_TOOLS_PATH}" | cut -f2 -d'-')
-    VMWARE_TOOLS_BUILD=$(echo "${VMWARE_TOOLS_PATH}" | cut -f3 -d'-')
-    VMWARE_TOOLS_BUILD=$(basename ${VMWARE_TOOLS_BUILD} .tar.gz)
-    echo "==> VMware Tools Path: ${VMWARE_TOOLS_PATH}"
-    echo "==> VMWare Tools Version: ${VMWARE_TOOLS_VERSION}"
-    echo "==> VMware Tools Build: ${VMWARE_TOOLS_BUILD}" 
-
-    tar zxf "$VMWARE_TOOLS_PATH" -C /tmp/
-    VMWARE_TOOLS_MAJOR_VERSION=$(echo ${VMWARE_TOOLS_VERSION} | cut -d '.' -f 1)
-    if [ "${VMWARE_TOOLS_MAJOR_VERSION}" -lt "10" ]; then
-        /tmp/vmware-tools-distrib/vmware-install.pl -d
-    else
-        /tmp/vmware-tools-distrib/vmware-install.pl -f
-    fi
-
-    VMWARE_TOOLBOX_CMD_VERSION=$(vmware-toolbox-cmd -v)
-    echo "==> Installed VMware Tools ${VMWARE_TOOLBOX_CMD_VERSION}" 
-
-    umount /mnt
-  fi
-fi
+aptitude install -y open-vm-tools
+mkdir -p /mnt/hgfs
