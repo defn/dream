4cc09b3882341f026faa98cd52418e706d600abd
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-07 11:54:18 -0700

    load interfaces.d

--- script/network.sh
+++ script/network.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-true
+if ! grep -s /etc/network/interfaces.d /etc/network/interfaces; then
+  echo 'source-directory /etc/network/interfaces.d' | tee -a /etc/network/interfaces
+fi
