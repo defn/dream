3a9b56dab4ebde7bf9e468c22a79e6b615f6db8c
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-07 12:36:21 -0700

    always insert a blank line

--- script/network.sh
+++ script/network.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
 if ! grep -s /etc/network/interfaces.d /etc/network/interfaces; then
-  echo 'source-directory /etc/network/interfaces.d' | tee -a /etc/network/interfaces
+  {
+    echo
+    echo 'source-directory /etc/network/interfaces.d'
+  } | tee -a /etc/network/interfaces
 fi
