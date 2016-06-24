ce5e246ef6a0589f9c734867f5a416ba04a0e246
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-24 14:20:42 -0400

    Revert "always add vagrant insecure key to virtualbox"
    This reverts commit 7e089f77e5ec3be5debfd4869ef470dada51e21b.

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -23,8 +23,3 @@ sh /mnt/VBoxLinuxAdditions.run || true
 echo "==> Installed VirtualBox Guest Additions ${VBOX_VERSION}" 
 
 umount /mnt
-
-install -d -o ubuntu -g ubuntu ~ubuntu/.ssh
-touch ~ubuntu/.ssh/authorized_keys
-chown ubuntu:ubuntu ~ubuntu/.ssh/authorized_keys
-echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key' >> ~ubuntu/.ssh/authorized_keys
