70850c56345071f6f572073b3c7280205e950e4b
Tom Bombadil <amanibhavam@destructuring.org>
2016-06-24 14:10:16 -0400

    exit early

--- script/virtualbox.sh
+++ script/virtualbox.sh
@@ -1,21 +1,25 @@
-#!/usr/bin/env bash -eux
+#!/usr/bin/env bash
 
-set -eux
+set -exfu
 
 echo "==> Installing VirtualBox guest additions"
 
-pth_vbox_version="$(ls -d /home/*/.vbox_version 2>/dev/null || true)"
-if [[ -f "$pth_vbox_version" ]]; then
-  cd "$(dirname "$pth_vbox_version")"
-  VBOX_VERSION=$(cat .vbox_version)
-
-  if [[ -f "VBoxGuestAdditions_$VBOX_VERSION.iso" ]]; then
-    mount -o loop "$(pwd)/VBoxGuestAdditions_$VBOX_VERSION.iso" /mnt
-
-    # install
-    sh /mnt/VBoxLinuxAdditions.run || true
-    echo "==> Installed VirtualBox Guest Additions ${VBOX_VERSION}" 
-
-    umount /mnt
-  fi
+pth_vbox_version="$(set +f; ls -d /home/*/.vbox_version 2>/dev/null || true)"
+if [[ ! -f "$pth_vbox_version" ]]; then
+  exit 0
 fi
+
+cd "$(dirname "$pth_vbox_version")"
+VBOX_VERSION=$(cat .vbox_version)
+
+if [[ ! -f "VBoxGuestAdditions_$VBOX_VERSION.iso" ]]; then
+  exit 0
+fi
+
+mount -o loop "$(pwd)/VBoxGuestAdditions_$VBOX_VERSION.iso" /mnt
+
+# install
+sh /mnt/VBoxLinuxAdditions.run || true
+echo "==> Installed VirtualBox Guest Additions ${VBOX_VERSION}" 
+
+umount /mnt
