5593fcf77ffd6450473d2be901cc8697e28b76e5
Tom Bombadil <amanibhavam@destructuring.org>
2016-08-22 13:29:04 -0700

    no zfs on trusty, too weird

--- script/site.sh
+++ script/site.sh
@@ -9,9 +9,7 @@ aptitude install -y software-properties-common xfsprogs
 case "$DISTRIB_CODENAME" in
   trusty)
     add-apt-repository ppa:ubuntu-lxc/lxd-stable
-    apt-add-repository -y ppa:zfs-native/stable
     aptitude update
-    aptitude install -y ubuntu-zfs dkms
     ;;
 
   xenial)
