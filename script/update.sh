06345b7742d2f403cadbdcdee3846c6b0fcbba45
Tom Bombadil <amanibhavam@destructuring.org>
2016-08-27 18:17:13 -0700

    dont do null builder yet

--- script/update.sh
+++ script/update.sh
@@ -10,7 +10,7 @@ if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
 	aptitude install -y linux-generic-lts-xenial
 fi
 
-if [[ ! $PACKER_BUILDER_TYPE =~ docker|null ]]; then
+if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
 	reboot
 	sleep 120
 fi
