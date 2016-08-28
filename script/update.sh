fa565c47b455f6f5231b5be3269043517f27c450
Tom Bombadil <amanibhavam@destructuring.org>
2016-08-28 14:40:45 -0700

    only reboot during an iso build

--- script/update.sh
+++ script/update.sh
@@ -6,11 +6,8 @@ umask 022
 aptitude dist-upgrade -y
 aptitude upgrade -y
 
-if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
+if [[ $PACKER_BUILDER_TYPE =~ iso ]]; then
 	aptitude install -y linux-generic-lts-xenial
-fi
-
-if [[ ! $PACKER_BUILDER_TYPE =~ docker ]]; then
 	reboot
 	sleep 120
 fi
