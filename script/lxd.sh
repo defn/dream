46bdadd40bfa2707eee6e883ce6d966ba805509a
Tom Bombadil <amanibhavam@destructuring.org>
2016-10-08 01:24:08 -0700

    let cloud-init install lxd

--- script/lxd.sh
+++ script/lxd.sh
@@ -3,17 +3,10 @@
 function main {
   set -exfu
 
-  export DEBIAN_FRONTEND=noninteractive
-
-  add-apt-repository ppa:ubuntu-lxc/lxd-stable
-  aptitude update
-  aptitude install -y lxd
-
-	usermod -aG lxd ubuntu
-
-  mkfs.btrfs /dev/inception/lxd
-  mkdir -p /var/lib/lxd
-  printf 'UUID=%s /var/lib/lxd btrfs user_subvol_rm_allowed\n' "$(blkid /dev/inception/lxd | cut -d\" -f2)" >> /etc/fstab
+  if mkfs.btrfs /dev/inception/lxd; then
+    mkdir -p /var/lib/lxd
+    printf 'UUID=%s /var/lib/lxd btrfs user_subvol_rm_allowed\n' "$(blkid /dev/inception/lxd | cut -d\" -f2)" >> /etc/fstab
+  fi
 }
 
 main "$@"
